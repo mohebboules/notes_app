758cb4cc8fd1fedcdc589d724116a36e39895ced
Moheb Boules Guirges <mohebboules@gmail.com>
2025-01-21 21:14:12 +0200

    Create custom NoteItem

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+        right: 16,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                  color: Colors.black.withValues(alpha: 0.5),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                  size: 24,
+                )),
+          ),
+          Text(
+            'May 21, 2022',
+            style: TextStyle(
+              color: Colors.black.withValues(alpha: 0.6),
+              fontSize: 16,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
